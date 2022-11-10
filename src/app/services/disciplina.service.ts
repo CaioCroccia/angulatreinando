fc3d563834989fc4a24fc53f6fa78bd8625c6ec3
CaioCroccia <103905336+CaioCroccia@users.noreply.github.com>
2022-11-10 13:27:06 -0300

    1 file modified, 1 added

--- src/app/services/disciplina.service.ts
+++ src/app/services/disciplina.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@angular/core';
 
+interface disciplina{
+  title: string;
+  name: string;
+  hora: string;
+}
+
 @Injectable()
 export class DisciplinaService {
 
   constructor() { }
+   lista: Array<disciplina> = [
+      {
+        title: 'a',
+        name: 'oi',
+        hora:  '12-14:30'
 
+      }
+  ]
 }
