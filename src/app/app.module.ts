f8960b54ed6052dd1f3b94e7a20c7ce628e10c29
CaioCroccia <103905336+CaioCroccia@users.noreply.github.com>
2022-11-09 23:30:01 -0300

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { HomeComponent } from './components/home/home.component';
 import { DisciplinaComponent } from './components/disciplina/disciplina.component';
 import { TimerComponent } from './components/timer/timer.component';
 import { RouterModule } from '@angular/router';
+import { TimerService } from './services/timer.service';
+import { DisciplinaService } from './services/disciplina.service';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, RouterModule.forRoot([
@@ -17,6 +19,7 @@ import { RouterModule } from '@angular/router';
 
   ]) ],
   declarations: [ AppComponent, HelloComponent, HomeComponent, DisciplinaComponent, TimerComponent ],
-  bootstrap:    [ AppComponent ]
+  bootstrap:    [ AppComponent ],
+  providers: [TimerService, DisciplinaService]
 })
 export class AppModule { }
