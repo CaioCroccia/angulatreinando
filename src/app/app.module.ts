6614d60f0164d484c5535db319adf5f9a4808913
CaioCroccia <103905336+CaioCroccia@users.noreply.github.com>
2022-11-09 23:24:06 -0300

    3 files modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { HomeComponent } from './components/home/home.component';
+import { DisciplinaComponent } from './components/disciplina/disciplina.component';
+import { TimerComponent } from './components/timer/timer.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, HomeComponent, DisciplinaComponent, TimerComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
